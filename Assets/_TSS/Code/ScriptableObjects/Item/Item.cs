b2ac3e6c696da35dd0ecd6447c04d64d88feb588
Dante <martin.velazquez.luis.alfonso@gmail.com>
2025-05-10 14:15:54 -0600

    Item Script Created

---
+++ Assets/_TSS/Code/ScriptableObjects/Item/Item.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace _TSS.Item
+{
+    [CreateAssetMenu(fileName = "ItemFile", menuName = "_TSS/Item", order = 0)]
+    public class Item : ScriptableObject
+    {
+        [field: SerializeField] public string Name { get; private set; }
+        [field: SerializeField] public Sprite Icon { get; private set; }
+        [field: SerializeField] public int Price { get; private set; }
+        
+        [field: TextArea]
+        [field: SerializeField] public string Description { get; private set; }
+    }
+}
